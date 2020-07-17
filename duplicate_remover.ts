7008c5c83900542a966d1428e34eb165907e8d9a
tomholford <16504501+tomholford@users.noreply.github.com>
2020-07-17 16:46:58 +0900

    Remove duplicates from output dir (#1)
    * generate map of hash : filepaths
    
    * duplicate_remove remove dupes :)
    
    * add CLI flag to enable feature
    
    Co-authored-by: tomholford <tomholford@users.noreply.github.com>

---
+++ duplicate_remover.ts
@@ -0,0 +1,63 @@
+// import {  } from 'https://deno.land/std/hash/mod.ts';
+
+import { exists, walkSync, WalkEntry } from "https://deno.land/std/fs/mod.ts";
+import { join } from "https://deno.land/std/path/mod.ts";
+import FileHasher from './file_hasher.ts';
+import asyncForEach from './async_for_each.ts';
+
+/**
+ * Removes duplicate files within a directory 
+ */
+class DuplicateRemover {
+  directory: string;
+
+  constructor(directory: string) {
+    this.directory = directory;
+  }
+
+  async removeDuplicates() {
+    console.log(`removing duplicates from ${this.directory} ...`)
+    let duplicateSet: Set<string> = new Set();
+    const paths = await this.paths();
+
+    paths.forEach((path: string, _i, _a) => {
+      const hash = FileHasher.md5(path);
+      if(duplicateSet.has(hash)) {
+        console.log(`Found duplicate at ${path}, removing ...`);
+        if(exists(path)) {
+          Deno.removeSync(path)
+        } else {
+          console.log(`Unable to remove ${path}`)
+        }
+      } else {
+        duplicateSet.add(hash);
+      }
+    })
+  }
+
+  private async paths(): Promise<string[]> {
+    let output: string[] = [];
+
+    const entries = Array.from(walkSync(this.directory));
+
+    await asyncForEach(entries, async (entry: WalkEntry) => {
+      const isDir = await this.isDirectory(entry.path);
+
+      if(!isDir) {
+        output.push(entry.path);
+      }
+    })
+
+    return output;
+  }
+
+  private async isDirectory(path: string): Promise<boolean> {
+    const f = await Deno.open(path, { read: true, write: false });
+    const fileinfo = await Deno.fstat(f.rid);
+    f.close();
+
+    return fileinfo.isDirectory;
+  }
+}
+
+export default DuplicateRemover;
